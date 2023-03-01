4f199efcfabd4749731ae11b7a32673a1d0cd0cc
maiquytan <113486856+maiquytan@users.noreply.github.com>
2023-03-01 11:38:14 +0700

    add About Us screen

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Request from "./Request/Request";
 
 const Layout = ({ children }) => {
   return (
     <div className="content">
       <Header/>
       {children}
+      <Request/>
       <Footer />
     </div>
   );
--- pages/_app.js
+++ pages/_app.js
@@ -27,6 +27,9 @@ function App({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link async href="https://fonts.googleapis.com/css2?family=Lexend:wght@400;700;800;900&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link async href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500;700&display=swap" rel="stylesheet"></link>
       </Head>
       <Layout>
         <Component {...pageProps} />
